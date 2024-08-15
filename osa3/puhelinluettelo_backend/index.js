462b0935450989cd7f8208eafd9d10846ff1ab98
o <onni.laukkala@helsinki.fi>
2024-08-15 10:31:06 +0300

    number validation on update final fix

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -83,10 +83,10 @@ app.post('/api/persons', (request, response, next) => {
 app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
 
-  const person = new Person({
+  const person = {
     name: body.name,
     number: body.number
-  })
+  }
 
   Person.findByIdAndUpdate(request.params.id, person, { new:true, runValidators:true, context:'query' })
     .then(updatedPerson => {
