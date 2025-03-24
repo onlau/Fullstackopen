12421aef40f7ff474f39d9df03194f6b376b20ba
o <onni.laukkala@helsinki.fi>
2025-03-24 13:12:27 +0200

    4.18

---
+++ osa4/blogilista/controllers/login.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const user = require('../models/user')
+const User = require('../models/user')
+
+loginRouter.post('/', async (request,response) => {
+    const {username, password} = request.body
+
+    const user = await User.findOne({username})
+    const passCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.passHash)
+    
+    if (!(user&&passCorrect)){
+        return response.status(401).json({
+            error: "invalid username or password"
+        })
+    }
+
+    const tokenUser = {
+        user: username,
+        id: user._id
+    }
+
+    const token = jwt.sign(tokenUser, process.env.SECRET)
+
+    response.status(200).send({token, username: user.username, name: user.name})
+})
+
+module.exports = loginRouter
